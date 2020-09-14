7fa68832c239e39db686ed263f4f6282546d7bd1
volumes <vol.umes@mail.ru>
2020-09-14 19:18:07 +0300

    add input

---
+++ src/components/Input.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./Input.sass"
+
+export const Input = () => {
+    return (
+        <>
+            <div className="input-cont">
+                <input type="text"placeholder="Input some text..." className="input"/>
+            </div>
+        </>
+    )
+}
